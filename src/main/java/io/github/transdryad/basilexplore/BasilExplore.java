97127ab3a3945572ed0861eb87b166a7a0ce8325
Hazel Viswanath <viswanathsam@gmail.com>
2024-06-22 14:34:02 -0600

    Create EventListener Class

--- src/main/java/io/github/transdryad/basilexplore/BasilExplore.java
+++ src/main/java/io/github/transdryad/basilexplore/BasilExplore.java
@@ -7,6 +7,7 @@ public final class BasilExplore extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getServer().getPluginManager().registerEvents(new BasilListener(), this);
 
     }
 
---
+++ src/main/java/io/github/transdryad/basilexplore/BasilListener.java
@@ -0,0 +1,7 @@
+package io.github.transdryad.basilexplore;
+
+import org.bukkit.event.Listener;
+
+public class BasilListener implements Listener {
+
+}
